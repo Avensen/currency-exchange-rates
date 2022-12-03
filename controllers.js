8662f9386c536102fc091eb08a4e2f22b08e3167
Avensen Nejersian <anejersian@gmail.com>
2022-12-03 10:33:59 -0500

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,16 +1,13 @@
 const axios = require("axios").default;
 
 const headers = new Headers({
-	Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2OTg2ODEwNSwianRpIjoiM2UyNDVmMjUtYWYxYS00MTI0LWJlZDAtY2Y4ZDk2NDdhNzgyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjYzODgyYTQ5ZTNhYzlkNWM2NDcwOTZiYiIsIm5iZiI6MTY2OTg2ODEwNSwiZXhwIjoxNzAxNDA0MTA1fQ.ZSX1HsOBfWtc-ywjH7bnadQzHAEKmawaAMyj7Ha8bRw",
+	Authorization: "Bearer " + process.env.API_KEY
 });
 
 async function getRates() {
 	try {
 		const response = await axios.get(
-			"https://tasas.eltoque.com/v1/trmi?date_from=2022-12-03%2000%3A00%3A01&date_to=2022-12-03%2023%3A59%3A01",
-         {
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2OTg2ODEwNSwianRpIjoiM2UyNDVmMjUtYWYxYS00MTI0LWJlZDAtY2Y4ZDk2NDdhNzgyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjYzODgyYTQ5ZTNhYzlkNWM2NDcwOTZiYiIsIm5iZiI6MTY2OTg2ODEwNSwiZXhwIjoxNzAxNDA0MTA1fQ.ZSX1HsOBfWtc-ywjH7bnadQzHAEKmawaAMyj7Ha8bRw",
-         }
+			"https://tasas.eltoque.com/v1/trmi?date_from=2022-12-03%2000%3A00%3A01&date_to=2022-12-03%2023%3A59%3A01", headers
 		);
 		console.log(response);
 	} catch (error) {
